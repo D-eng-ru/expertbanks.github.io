b318f9687e1c2df94e1d8c48466184b4c66b2bba
adasils <91526030+adasils@users.noreply.github.com>
2023-01-23 18:06:23 +0100

    new blocks, fixed

---
+++ js/script.js
@@ -0,0 +1,46 @@
+    window.sr = ScrollReveal();
+        sr.reveal('.navigation', {
+            duration: 2000,
+            distance: '20px',
+            origin: 'bottom'
+        }); 
+        sr.reveal('.blk1', {
+            duration: 2000,
+            distance: '20px',
+            origin: 'left'
+        }); 
+        sr.reveal('.blk1_1', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'right'
+        }); 
+        sr.reveal('.block-2', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'bottom'
+        }); 
+        sr.reveal('.blk3', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'left'
+        }); 
+          sr.reveal('.blk3_image', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'right'
+        }); 
+          sr.reveal('.block4-title', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'bottom'
+        }); 
+          sr.reveal('.services-card', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'bottom'
+        }); 
+          sr.reveal('footer', {
+            duration: 2000,
+            distance: '30px',
+            origin: 'bottom'
+        }); 
